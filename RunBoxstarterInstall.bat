7028bbb6a62270fa0bf9303c27a25cc86b069fa6
Jeffrey <jeffrey.jeffrey25@gmail.com>
2019-06-28 23:00:28 -0700

    Update RunBoxstarterInstall.bat

--- RunBoxstarterInstall.bat
+++ RunBoxstarterInstall.bat
@@ -1,1 +1,1 @@
-START http://boxstarter.org/package/url?https://gist.githubusercontent.com/jeffrey25/29682a06c875f8bbd3289b84ee966817/raw/f0e3b5e748633a49c63d64a212a300283be281dc/boxstarter_jejoh_full.txt
+START http://boxstarter.org/package/url?https://raw.githubusercontent.com/jeffrey25/Boxstarter/master/jejoh_full.txt
